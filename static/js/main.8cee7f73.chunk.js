262b5d02aebfeb77b44edca9a6ad7cfa11791a7d
Raphael Suter <raphael.werner.suter@gmail.com>
2021-03-20 20:06:26 +0100

    Updates

--- static/js/main.8cee7f73.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonp152_frontend=this.webpackJsonp152_frontend||[]).push([[0],{4:function(e,n,t){"use strict";t.r(n);t(0);var r=t(3),o=t.n(r),c=t(2),s=function(){return Object(c.jsx)("h1",{children:"Hello World"})};o.a.render(Object(c.jsx)(s,{}),document.getElementById("root"))}},[[4,1,2]]]);
-//# sourceMappingURL=main.8cee7f73.chunk.js.map
