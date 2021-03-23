b53d5f07a94f2475ffb673d232168e23fe38b22b
Raphael Suter <raphael.werner.suter@gmail.com>
2021-03-23 16:17:34 +0100

    ...

--- src/pages/Docs.js
+++ src/pages/Docs.js
@@ -18,16 +18,21 @@ const Docs = () => (
     />
     <Separator color1="#ffc107" color2="black" />
     <Separator color1="black" color2="white" />
-      <Container color="white">
-          <Title color="black">Entscheidungen</Title>
-          <Photo src="img/Styling.png" />
-          <Photo src="img/Framework.png" />
-          <Photo src="img/IDE.png" />
-      </Container>
+    <Container color="white">
+      <Title color="black">Entscheidungen</Title>
+      <Photo src="img/Styling.png" />
+      <Photo src="img/Framework.png" />
+      <Photo src="img/IDE.png" />
+    </Container>
     <Separator color1="white" color2="black" />
     <Container color="black">
       <Title color="white">Externe Module</Title>
-      <Text color="white">Lorem Ipsum</Text>
+      <Text color="white">
+        <li>react-bootstrap-icons</li>
+        <li>react-router-dom</li>
+        <li>styled-components</li>
+        <li>gh-pages</li>
+      </Text>
     </Container>
     <Separator color1="black" color2="#ffc107" />
     <Footer />
